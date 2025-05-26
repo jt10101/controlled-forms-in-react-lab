aad975eb2dbc4321392fc2b6bb91db5dd6affb86
Jacob Tham <jacobtham3@gmail.com>
2025-05-26 20:59:19 +0800

    adding form

--- src/components/Bookshelf/Bookshelf.jsx
+++ src/components/Bookshelf/Bookshelf.jsx
@@ -1,11 +1,19 @@
 import { useState } from "react";
 
+startingBooks = [
+  { title: "Fourth Wing", author: "Rebecca Yarros" },
+  { title: "The Lion, the Witch and the Wardrobe", author: "C.S. Lewis" },
+];
+
 const BookShelf = () => {
+  const [books, setBooks] = useState(startingBooks);
   return (
     <div className="bookshelfDiv">
       <div className="formDiv">
         <h3>Add a Book</h3>
-        {/* Form will go here */}
+        <form>
+          <input type="text" name="bookTitle"></input>
+        </form>
       </div>
       <div className="bookCardsDiv">{/* Book cards will display here */}</div>
     </div>
